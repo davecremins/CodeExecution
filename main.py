41fdee3d788a094a707107230ccc5689184208f5
Dave Cremins <dave.cremins@wpengine.com>
2017-09-11 14:54:26 +0100

    Added command for bashing into container

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def execute_cmd(command=["docker", "ps"]):
     call(command)
 
 if(__name__ == '__main__'):
-    execute_cmd()
+    execute_cmd(["docker", "run", "-it", "python_and_docker", "/bin/bash"])
