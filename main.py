b565e3028fb623b0e013d5e2597bdb63356cb2d1
Dave Cremins <dave.cremins@wpengine.com>
2017-09-11 17:09:52 +0100

    Small refactor and simple django app

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from subprocess import call
-from utilities.timer import Timer
+from utilities.timer import CommandTimer as CT
 
 def execute_cmd(command=["docker", "ps"]):
     call(command)
 
 if(__name__ == '__main__'):
-    with Timer() as t:
+    with CT():
         execute_cmd(["docker", "run", "--rm", "-it", "python_and_docker", "/bin/bash"])
-    print(f'Command lasted: {t.elapsed_in_secs} secs')
